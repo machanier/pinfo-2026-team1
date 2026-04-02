0997da8bc00e4d2f9fc6e22002fc1cf11241b0db
Iris Riedo <iris.riedo@etu.unige.ch>
2026-04-02 19:07:35 +0000

    PINFO-130, PINFO-131: implémentation endpoints association

--- backend/user-service/src/main/java/ch/unige/pinfo/user/model/Association.java
+++ backend/user-service/src/main/java/ch/unige/pinfo/user/model/Association.java
@@ -1,9 +1,18 @@
 package ch.unige.pinfo.user.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Association extends User {
+    @Column(nullable = false)
     public String description;
-    public Boolean verified;// to remove
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
